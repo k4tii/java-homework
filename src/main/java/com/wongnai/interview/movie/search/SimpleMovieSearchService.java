de9d488012094b791a00877d7c28249ca04951af
k4tii <sudtida.ponvijit@gmail.com>
2019-09-21 23:59:44 +0700

    Update step 2

--- src/main/java/com/wongnai/interview/movie/search/SimpleMovieSearchService.java
+++ src/main/java/com/wongnai/interview/movie/search/SimpleMovieSearchService.java
@@ -1,7 +1,12 @@
 package com.wongnai.interview.movie.search;
 
+import java.lang.reflect.Array;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import com.wongnai.interview.movie.external.MovieData;
+import com.wongnai.interview.movie.external.MoviesResponse;
+import org.assertj.core.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +24,16 @@ public class SimpleMovieSearchService implements MovieSearchService {
 		//TODO: Step 2 => Implement this method by using data from MovieDataService
 		// All test in SimpleMovieSearchServiceIntegrationTest must pass.
 		// Please do not change @Component annotation on this class
-		return null;
+		MoviesResponse moviesResponse = movieDataService.fetchAll();
+		List<Movie> movieList = moviesResponse.stream()
+				.filter(movieData -> Arrays.asList(movieData.getTitle().toLowerCase().split("\\s+")).contains(queryText.toLowerCase()))
+				.collect(Collectors.mapping(movieData -> mapMovieDataToMovie(movieData),Collectors.toList()));
+		return movieList;
+	}
+
+	private Movie mapMovieDataToMovie(MovieData movieData){
+		Movie movie = new Movie(movieData.getTitle());
+		movie.getActors().addAll(movieData.getCast());
+		return movie;
 	}
 }
